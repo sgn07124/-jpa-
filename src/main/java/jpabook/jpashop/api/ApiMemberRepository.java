cf5dda54a2cd3de82d0b226575a69697fd309373
Kim Kyeongho <sgn07124@gmail.com>
2024-08-08 01:45:06 +0900

    회원 수정 API

--- src/main/java/jpabook/jpashop/api/ApiMemberRepository.java
+++ src/main/java/jpabook/jpashop/api/ApiMemberRepository.java
@@ -4,11 +4,10 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,7 +25,7 @@ public class ApiMemberRepository {
      * 결론 :
      * - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
      */
-    @PostMapping("api/v1/members")
+    @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {  // 이렇게 Member를 외부에 노출시키면 안되며 DTO를 파라미터로 받아야 함
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
@@ -40,13 +39,41 @@ public class ApiMemberRepository {
      * - 엔티티와 API 스펙을 명확하게 분리할 수 있다.
      * - 엔티티가 변해도 API 스펙이 변하지 않는다.
      */
-    @PostMapping("api/v2/members")
+    @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {  // @RequestBody에 dto를 매핑
         Member member = new Member();
         member.setName(request.name);
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    /**
+     * 회원 정보 수정 v2 : 회원 정보를 부분 수정한다.
+     * 특징 :
+     * - PUT 방식 : 전체 업데이트를 할 때 사용
+     * - PATCH(or POST) 방식 : 부분 업데이트를 할 때 사용
+     */
+    @PatchMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {  // DTO를 파라미터에 매핑
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data  // DTO는 데이터가 왔다갔다 하기 때문에 @Data를 써도 되지만 엔티티는 생각하고 써야된다.
+    @AllArgsConstructor
+    static class UpdateMemberRequest {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 
     @Data
