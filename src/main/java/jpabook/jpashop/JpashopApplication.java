5e194045ec6dca3ed304a62f6abc2c87073930bf
Kim Kyeongho <sgn07124@gmail.com>
2024-08-10 00:58:16 +0900

    주문 조회 API - 엔티티를 직접 노출

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -14,6 +16,18 @@ public class JpashopApplication {
 		System.out.println("data = " + data);
 
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	/**
+	 * V1 - Hibernate5JakartaModule 등록
+	 * 기본적으로 초기화 된 프록시 객체만 노출. 초기화 되지 않은 프록시 객체는 노출 안함
+	 */
+	@Bean
+	Hibernate5JakartaModule hibernate5Module() {
+		Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+		// 강제 지연 로딩 설정
+		//hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5JakartaModule;
 	}
 
 }
---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleAPIController.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * XToOne(ManyToOne, OneToOne)
+ * - Order
+ * - Order -> Member
+ * - Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleAPIController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5JakartaModule 모듈 등록. LAZY=null 처리
+     * 주의 및 참고 :
+     * - 양방향 관계 문제 발생 -> 엔티티를 직접 노출할 때는 양방향 연관관계가 걸린 곳 중 한 곳에 @JsonIgnore 처리해야 됨.(추가하지 않으면 양쪽에서 서로 후출하면서 무한루프 발생함)
+     * - 엔티티를 API 응답으로 외부로 노출하면 안됨. -> Hibernate5JakartaModule를 사용하는 것 보다는 DTO로 변환해서 반환하는 것이 좋음
+     * - 항상 지연 로딩(LAZY)를 기본으로 설정해라. -> 즉시 로딩(EAGER) 때문에 연관관계가 필요 없는 경우에도 데이터를 항상 조회해서 성능 문제 발생 가능성이 있다.
+     *    따라서 성능 최적화가 필요한 경우에는(fetch join)을 사용할 것(v3에서 설명)
+     */
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();  // Lazy 강제 초기화
+            order.getDelivery().getAddress();  // Lazy 강제 초기화
+        }
+        return all;
+    }
+}
